73d967b6cf991dee838656737db4bb793f5e0830
Harikrishnan_B_wac_react <harikrishnan.b@webandcrafts.in>
2025-03-13 09:59:47 +0530

    page not found fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
   server: {port: 5174},
   alias: {
     '@': '/src', // Ensure this points to your src directory
